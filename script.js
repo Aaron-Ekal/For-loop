1482b1074974a02632175e428a9135191debd7ac
Aaron Ekal <ekalaaronkalale@gmail.com>
2022-03-07 20:20:13 +0300

    add for in loop

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ for(let i=10; i>0; i--){
     console.log(i)
 }
 console.log('HAPPY NEW YEAR!')
+
+//For in Loop
+let letters = ["H","E","L","P"]
+for(let i in letters){
+    console.log(letters[i])
+}
